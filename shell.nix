69f170a2e4c9ca15ddab6b61c30f33d199bc8ccb
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2021-07-11 01:27:42 +0300

    Nix: Provide Perl dependencies in “shellHook”

--- shell.nix
+++ shell.nix
@@ -2,9 +2,17 @@
 # License: GNU/GPLv3 https://raw.githubusercontent.com/unclechu/gpaste-gui/master/LICENSE
 let sources = import nix/sources.nix; in
 { pkgs ? import sources.nixpkgs {}
+, providePerlDependenciesForDevShell ? true
 }:
 let gpaste-gui = pkgs.callPackage ./. {}; in
 pkgs.mkShell {
+  shellHook = if providePerlDependenciesForDevShell then ''
+    export PERL5LIB=${
+      pkgs.lib.escapeShellArg
+        (pkgs.perlPackages.makePerlPath gpaste-gui.perlDependencies)
+    } || exit
+  '' else "";
+
   buildInputs = [
     gpaste-gui
   ];
